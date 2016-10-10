6f2b7f7123fe4f0082ec1cfb9cfd78bfe1b45211
Jakub Pierewoj <jp360641@students.mimuw.edu.pl>
2016-10-10 12:51:42 +0200

    wczytywanie zapytan

--- main.cpp
+++ main.cpp
@@ -19,59 +19,71 @@ struct PoststampCompare
 using PoststampStore = std::multiset<Poststamp, PoststampCompare>;
 using PoststampStoreIter = std::multiset<Poststamp, PoststampCompare>::iterator;
 
-void PrintPoststamp(const Poststamp& poststamp)
+namespace
 {
-    std::cout<<
-        std::get<0>(poststamp)<<" "<<
-        std::get<4>(poststamp)<<" "<<
-        std::get<1>(poststamp)<<" "<<
-        std::get<3>(poststamp)<<"\n";
-}
+  void PrintPoststamp(const Poststamp& poststamp)
+  {
+      std::cout<<
+          std::get<0>(poststamp)<<" "<<
+          std::get<4>(poststamp)<<" "<<
+          std::get<1>(poststamp)<<" "<<
+          std::get<3>(poststamp)<<"\n";
+  }
 
-void ProcessLine(const PoststampStore store, const std::string& line)
-{
-    //TODO
-}
+  bool ProcessStamp(const PoststampStore store, const std::string& line)
+  {
+      return true;
+  }
 
-Poststamp BuildPoststamp(int year, double value, std::string country, 
-    std::string name, std::string valueInput)
-{
-    Poststamp poststamp;
-    std::get<0>(poststamp) = year;
-    std::get<1>(poststamp) = value;
-    std::get<2>(poststamp) = country;
-    std::get<3>(poststamp) = name;
-    std::get<4>(poststamp) = valueInput;
-    return poststamp;
-}
+  void ProcessQuery(const PoststampStore& store, const std::string& line)
+  {
 
-void RegisterPoststamp(PoststampStore& store, const Poststamp& poststamp)
-{
-     store.insert(poststamp);
-}
+  }
 
-void QueryPoststamps(const PoststampStore& store, int yearFrom, int yearTo)
-{
-    Poststamp begin = BuildPoststamp(yearFrom, 0, "", "", "");
-    Poststamp end = BuildPoststamp(yearTo, 0, "", "", "");
-    PoststampStoreIter fromIter = store.lower_bound(begin);
-    PoststampStoreIter toIter = store.upper_bound(end);
-    
-    std::list<Poststamp> result(fromIter, toIter);  
-    result.sort();
-    for(std::list<Poststamp>::iterator it = result.begin();
-        it != result.end(); it ++)
-    {
-        PrintPoststamp(*it);
-    }
+  Poststamp BuildPoststamp(int year, double value, std::string country,
+      std::string name, std::string valueInput)
+  {
+      Poststamp poststamp;
+      std::get<0>(poststamp) = year;
+      std::get<1>(poststamp) = value;
+      std::get<2>(poststamp) = country;
+      std::get<3>(poststamp) = name;
+      std::get<4>(poststamp) = valueInput;
+      return poststamp;
+  }
+
+  void RegisterPoststamp(PoststampStore& store, const Poststamp& poststamp)
+  {
+       store.insert(poststamp);
+  }
+
+  void QueryPoststamps(const PoststampStore& store, int yearFrom, int yearTo)
+  {
+      Poststamp begin = BuildPoststamp(yearFrom, 0, "", "", "");
+      Poststamp end = BuildPoststamp(yearTo, 0, "", "", "");
+      PoststampStoreIter fromIter = store.lower_bound(begin);
+      PoststampStoreIter toIter = store.upper_bound(end);
+
+      std::list<Poststamp> result(fromIter, toIter);
+      result.sort();
+      for(std::list<Poststamp>::iterator it = result.begin();
+          it != result.end(); it ++)
+      {
+          PrintPoststamp(*it);
+      }
+  }
 }
 
 int main()
 {
     std::string line;
     PoststampStore store;
+    bool readingStamps = true;
     while(std::getline(std::cin, line))
     {
-        ProcessLine(store, line);
+      if(readingStamps)
+        readingStamps = ProcessStamp(store, line);
+      else
+        ProcessQuery(store, line);
     }
 }
